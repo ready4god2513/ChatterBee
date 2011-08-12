24f4ddcc53076f19f3e50e322d2bce158220be62
ready4god2513 <brandonh@ibethel.org>
2011-08-12 08:16:49 -0700

    Deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,11 @@
 set :application, "jegit"
-set :repository,  "git@github.com:ready4god2513/ChatterBee.git"
 
 set :scm, :git
-set :branch, "master"
+set :repository,  "git@github.com:ready4god2513/ChatterBee.git"
+set :branch, $1 if `git branch` =~ /\* (\S+)\s/m
+
+set :user, "user"
 set :deploy_via, :remote_cache
+set :use_sudo, false
 
 server "173.255.209.57", :app, :web, :db, primary: true

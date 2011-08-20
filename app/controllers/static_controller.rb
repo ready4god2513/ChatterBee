c4f98194cbfc5382bad0af2d59d979f8eddd6ba8
Brandon Hansen <brandonh@ibethel.org>
2011-08-19 23:06:47 -0700

    Conversation permalink

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -4,4 +4,10 @@ Jegit.controllers :static do
     erb "static/privacy".to_sym
   end
   
+  get :room, :with => :id do
+    load_room(params[:id])
+    @messages = @room.history(@pubnub)
+    erb "static/room".to_sym
+  end
+  
 end
